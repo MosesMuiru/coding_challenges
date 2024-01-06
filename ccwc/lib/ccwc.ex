127d355abfa3e478bf19847cf7d665717f04ee01
MosesMuiru <mosesmuiru9@gmail.com>
2024-01-06 15:31:03 +0300

    fixed the executation and the reading of the file

--- ccwc/lib/ccwc.ex
+++ ccwc/lib/ccwc.ex
@@ -7,22 +7,32 @@ defmodule Ccwc do
   def main(args \\ []) do
     args
     |> parse_args()
-
   end
 
   # this is for parsing arg
   def parse_args(args) do
-  # {opts, filename, _} =
+    # {opts, filename, _} =
     #  args
     # |> OptionParser.parse(switches: [help: :boolean])
     # {opts, filename}
 
     case OptionParser.parse(args, switches: [help: :boolean]) do
-      {opts, _, _} -> execute(opts[:help])
-      # {_, filename, _} -> executive(List.to_string(filename))
+      # {opts, _, _} -> execute(opts)
+      {opts, filename, _} ->
+        case opts[:help] do
+          true ->
+            execute(:help)
+
+          _ ->
+            filename
+            |> List.to_string()
+            |> execute()
+        end
+
+      _ ->
+        IO.puts("check you input")
     end
   end
-
 
   # this used to proocess the incomming args from the parse
   def out_put(cmd_args) do
@@ -32,19 +42,22 @@ defmodule Ccwc do
     end
   end
 
-
   # executive
-  def executive(:help) do
+  def execute(:help) do
     IO.puts """
-    this is the help
+    --help
+    for help
 
+    ./ccwc filename
+    this will read the file 
     """
   end
 
-  def executive(filename) do
+  def execute(filename) do
     filename
     |> read_file()
     |> format_content()
+    |> IO.puts()
   end
 
   def read_file(filename) do
