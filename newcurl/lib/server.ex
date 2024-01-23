77fa6140c7bdab2ee755d707d6f817629dc445ac
MosesMuiru <muirumoses9@gmail.com>
2024-01-24 00:09:18 +0300

    fixed the url issue

--- newcurl/lib/server.ex
+++ newcurl/lib/server.ex
@@ -1,10 +1,13 @@
 defmodule Server do
-    
+
+    # here you insert the port which is automatically run in the supervisor 
     def connect(default_port) do
        
         {:ok, socket} = :gen_tcp.listen( default_port, [:binary, active: false, reuseaddr: true])
+        IO.puts("stated the server at #{default_port} --> this is the pid")
         continues_connection(socket)
     end
+
     def continues_connection(socket) do
         {:ok, client_socket} = :gen_tcp.accept(socket)
         # should also
