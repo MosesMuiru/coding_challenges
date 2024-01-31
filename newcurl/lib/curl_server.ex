b4917a3ead0e3b86edd0f85ad504e9e345ece73b
MosesMuiru <muirumoses9@gmail.com>
2024-01-31 03:04:00 +0300

    figured out the problme

--- newcurl/lib/curl_server.ex
+++ newcurl/lib/curl_server.ex
@@ -17,7 +17,7 @@ defmodule CurlServer do
     #   _-> IO.puts("waah kuna shida hapa")
 
     # end
-    {:ok, socket} = :gen_tcp.listen(5400, opts)
+    {:ok, socket} = :gen_tcp.listen(5500, opts)
     IO.inspect socket
     socket
 
@@ -35,28 +35,33 @@ defmodule CurlServer do
     IO.puts("the data")
     IO.puts(data)
 
-    fetching_data(data)
-    |> send_resp(socket_pid)
+    fetching_data(data, socket_pid)
+    # |> send_resp(socket_pid)
+
 
 
   end
   # after receiving a request it should send a responce to the client him self
 
-  defp fetching_data(url) do
+  defp fetching_data(url, socket_pid) do
 
     case HTTPoison.get(url) do
       {_, %HTTPoison.Response{status_code: status_code, body: _body}} ->
         IO.puts("status code: #{status_code}")
+        send_resp(status_code, socket_pid)
+        # status_code
 
       {:ok, %HTTPoison.Response{status_code: 404}} ->
         IO.puts("Not found :(")
-
+        # status_code
       {:error, %HTTPoison.Error{reason: reason}} ->
         IO.inspect(reason)
+        reason
     end
   end
-    defp send_resp(fetched_data, socket_pid) do
-      IO.puts(fetched_data)
+    def send_resp(fetched_data, socket_pid) do
+      IO.inspect(fetched_data)
+      IO.puts("above is the fetched data")
       :gen_tcp.send(socket_pid, fetched_data)
       :gen_tcp.close(socket_pid)
       IO.puts("response has been sent")
